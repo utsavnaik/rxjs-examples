b9028089b830d91686cbce3462028a3317da1f86
utsav naik <naik.utsav@gmail.com>
2021-06-07 17:01:39 +0530

    initialized source code

---
+++ rxjs_first.ts
@@ -0,0 +1,40 @@
+import { Observable } from 'rxjs';
+
+const observer = {
+  next: (value: any) => console.log('next', value),
+  error: (error: any) => console.log('error', error),
+  complete: () => console.log('complete!')
+};
+
+const observable = new Observable(subscriber => {
+  let count = 0;
+  // Observables can deliver 0:M values synchronous or asynchronously
+  const id = setInterval(() => {
+    subscriber.next(count);
+    // calling complete also invokes the cleanup function you return
+    subscriber.complete();
+    count += 1;
+  }, 1000);
+
+  /*
+   * You can return a function to clean up any resources that were
+   * created with subscription. In this case, we need to clear 
+   * the active interval. When using RxJS's built in creation operators
+   * this will be handled for us.
+   */
+  return () => {
+    clearInterval(id);
+  };
+});
+
+// adding logs to show observable emitting asynchronously
+observable.subscribe(observer);
+
+function setInterval(arg0: () => void, arg1: number) {
+    throw new Error('Function not implemented.');
+}
+
+function clearInterval(id: void) {
+    throw new Error('Function not implemented.');
+}
+
